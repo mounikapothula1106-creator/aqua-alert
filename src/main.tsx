6c4425a06655eae88a4a3aeb9f1932e8ceaa42ea
Pothula Mounika <mounikapothula1106@gmail.com>
2025-09-20 18:33:16 +0530

    Refactor React import and update root rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
-import 'leaflet/dist/leaflet.css'; // Leaflet CSS import
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
