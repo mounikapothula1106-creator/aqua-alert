49873128cd6f2e2d184fb89f55ebf16ce5140c63
Pothula Mounika <mounikapothula1106@gmail.com>
2025-09-20 18:38:15 +0530

    Refactor imports and update render method
    Fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
+import 'leaflet/dist/leaflet.css'; // Leaflet CSS import
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
