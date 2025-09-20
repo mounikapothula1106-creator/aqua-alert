af91b29b1323a397f1eb2e764713e245f32d6342
Pothula Mounika <mounikapothula1106@gmail.com>
2025-09-20 18:21:41 +0530

    Refactor imports and render method in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import 'leaflet/dist/leaflet.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
+import 'leaflet/dist/leaflet.css'; // Leaflet CSS import
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
