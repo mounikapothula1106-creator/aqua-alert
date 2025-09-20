7a58a0ff234db37312882d4bdd2ecf1f18743a4f
Pothula Mounika <mounikapothula1106@gmail.com>
2025-09-20 16:12:59 +0530

    Create IoT Dashboard component

---
+++ IotDashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IotDashboard: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">🌍 IoT Disaster Alert Dashboard</h1>
+      <p>This is where your sensor data (pH, temperature, turbidity, etc.) will appear.</p>
+    </div>
+  );
+};
+
+export default IotDashboard;
